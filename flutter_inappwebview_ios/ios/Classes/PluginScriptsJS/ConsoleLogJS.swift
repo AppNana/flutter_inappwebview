aa47b907a4a012b9280300d0198e5e86b9f3192b
xiaomin.liu <395911263@qq.com>
2025-08-05 13:38:35 +0800

    Update ConsoleLogJS.swift
    解决yoti，年龄验证卡住的问题

--- flutter_inappwebview_ios/ios/Classes/PluginScriptsJS/ConsoleLogJS.swift
+++ flutter_inappwebview_ios/ios/Classes/PluginScriptsJS/ConsoleLogJS.swift
@@ -29,34 +29,59 @@ public class ConsoleLogJS {
     public static func CONSOLE_LOG_JS_SOURCE() -> String {
         return """
         (function(console) {
-        
             function _callHandler(logLevel, args) {
                 var message = '';
+
                 for (var i in args) {
                     try {
-                        message += message === '' ? args[i] : ' ' + args[i];
-                    } catch(_) {}
+                        let arg = args[i];
+
+                        // 对象转为 JSON 字符串（捕获异常）
+                        if (typeof arg === 'object') {
+                            try {
+                                arg = JSON.stringify(arg);
+                            } catch (e) {
+                                arg = '[Object]';
+                            }
+                        } else if (typeof arg === 'symbol') {
+                            arg = arg.toString(); // 防止直接拼接出错
+                        } else if (typeof arg === 'function') {
+                            arg = '[Function]';
+                        }
+
+                        message += message === '' ? arg : ' ' + arg;
+                    } catch (ignored) {
+                        message += ' [Unserializable]';
+                    }
                 }
+
+                // 调用原生 JS Bridge（捕获防止报错）
                 try {
-                    window.\(JavaScriptBridgeJS.get_JAVASCRIPT_BRIDGE_NAME()).callHandler('onConsoleMessage', {'level': logLevel, 'message': message})
-                } catch(_) {}
+                    window.\(JavaScriptBridgeJS.get_JAVASCRIPT_BRIDGE_NAME()).callHandler(
+                        'onConsoleMessage',
+                        {
+                            level: logLevel,
+                            message: message
+                        }
+                    );
+                } catch (_) {}
             }
-        
+
             var oldLogs = {
-                'consoleLog': console.log,
-                'consoleDebug': console.debug,
-                'consoleError': console.error,
-                'consoleInfo': console.info,
-                'consoleWarn': console.warn
+                consoleLog: console.log,
+                consoleDebug: console.debug,
+                consoleError: console.error,
+                consoleInfo: console.info,
+                consoleWarn: console.warn
             };
-        
+
             for (var k in oldLogs) {
                 (function(oldLog) {
                     var logLevel = oldLog.replace('console', '').toLowerCase();
                     console[logLevel] = function() {
-                        oldLogs[oldLog].apply(null, arguments);
-                        _callHandler(logLevel, arguments);
-                    }
+                        oldLogs[oldLog].apply(null, arguments); // 保留原输出
+                        _callHandler(logLevel, arguments);      // 通知原生
+                    };
                 })(k);
             }
         })(window.console);
